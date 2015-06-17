1fff90301e939aeb7955762d4a28aeb095560011
gabe0815 <gabriel.schweighauser@unibas.ch>
2015-06-17 16:45:04 +0200

    changed path

--- robot/wait_while_running.sh
+++ robot/wait_while_running.sh
@@ -3,6 +3,6 @@
 running=1
 echo "waiting for robot to complete current task"
 while [ $running = "1" ]; do
-	running=`/home/user/fanucrobot/KARELcomget.sh`
+	running=`/home/user/applications/RAPID/robot/KARELcomget.sh`
 done
 echo "robot completed task"
