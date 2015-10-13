627c400f1318916b23bd7b687fbd77b5cae495b1
gabe0815 <gabriel.schweighauser@unibas.ch>
2015-10-13 16:49:07 +0200

    adapted conversion script to be run as cronjob

--- analysis/convert_IMGseries_to_h264_recursive_metadata.sh
+++ analysis/convert_IMGseries_to_h264_recursive_metadata.sh
@@ -1,30 +1,69 @@
 #!/bin/bash
 
-while [ true ]
+#create a cron job which runs this script every hour. NOTE: only one instance of this script will run at a given time due to the pid file lock. The pid fil lock part was copied from here:
+#http://bencane.com/2015/09/22/preventing-duplicate-cron-job-executions/
+
+
+PIDFILE=/home/user/conversion.pid
+filePath=/mnt/4TBraid02/20151013_vibassay_test/
+
+
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+cd $filePath
+
+FILES=$(find ./ -name timestamp.txt)
+for file in $FILES
 do
-	FILES=$(find ./ -name timestamp.txt)
-	for file in $FILES
-	do
-		parentDir=`dirname $file`
-		outfile=$parentDir"/imgseries_h264.AVI"
-		timestampfile=$parentDir"/timestamp.txt"
-		metadatafile=$outfile"_metadata.txt"
-		qrimage=$outfile"_qrcode.jpg"
-		qrtext=$outfile"_qrcode.txt"
+		
+	parentDir=`dirname $file`
+	outfile=$parentDir"/imgseries_h264.AVI"
+	timestampfile=$parentDir"/timestamp.txt"
+	metadatafile=$outfile"_metadata.txt"
+	qrimage=$outfile"_qrcode.jpg"
+	qrtext=$outfile"_qrcode.txt"
 
-		if [ ! -f $outfile ]
-		then
-	    		echo "avconv is converting image series in "$dirname
-			avconv -f image2 -i `echo $parentDir"/IMG_%04d.JPG"` -qscale 0 -vcodec libx264 -an $outfile
-			echo "exiv2 is extracting metadata from image series in "$parentDir
-			exiv2 `echo $parentDir"/IMG_????.JPG"` > $metadatafile
-			echo "deleting original JPG files"
-			rm `echo $parentDir"/IMG_????.JPG"`
-			echo "done"
+	if [ ! -f $outfile ]
+	then
+    		echo "avconv is converting image series in "$dirname
+		avconv -f image2 -i `echo $parentDir"/IMG_%04d.JPG"` -qscale 0 -vcodec libx264 -an $outfile
+		echo "exiv2 is extracting metadata from image series in "$parentDir
+		exiv2 `echo $parentDir"/IMG_????.JPG"` > $metadatafile
+		echo "deleting original JPG files"
+		rm `echo $parentDir"/IMG_????.JPG"`
+		echo "done"
 
-		else 
-			echo "$outfile exists, skipping..."
-		fi
-	done
-	sleep 10
+	else 
+		echo "$outfile exists, skipping..."
+	fi
+
+
 done
+rm $PIDFILE
+
+
+
