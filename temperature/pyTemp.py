67e1048a64c17440023e7bddb2944598c00872f7
gabe0815 <g.schweighauser@gmail.com>
2015-10-21 13:08:35 +0200

    arduino temperature logging

---
+++ temperature/pyTemp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import serial
+from time import sleep
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
+ser.write("\x00")
+sleep(1);
+print ser.read(5)
+	
+ser.close()
