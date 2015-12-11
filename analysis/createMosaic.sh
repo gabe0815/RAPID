f3d8f696a1d48a64a5b21ff8b6485a17d6911168
gabe0815 <g.schweighauser@gmail.com>
2015-12-11 10:16:46 +0100

    added filter to exclude all sets recorded before a defined time point

--- analysis/createMosaic.sh
+++ analysis/createMosaic.sh
@@ -8,14 +8,26 @@ if [ -f $IMAGEPATH/sampleIDs.txt ]; then
     rm $IMAGEPATH/sampleIDs.txt ]
 fi
 
-for i in $(find $IMAGEPATH -name "sampleID.txt"); do echo -en $i'\t'; head -n1 $i; done >> $IMAGEPATH"/sampleIDs.txt"
+
+#exclude all sets that have been recorded before 2015-12-06 17:30:00 as until then, the assay was not working properly.
+#hence $BADTIME is the lower limit
+BADTIME=$(date --date="2015-12-06 17:30:00" +%s)
+
+for i in $(find $IMAGEPATH -name "sampleID.txt"); 
+do 
+	if [ $(tail -n1 $i) -ge $BADTIME ] then
+		echo -en $i'\t'; head -n1 $i; 
+	fi
+done >> $IMAGEPATH"/sampleIDs.txt"
 
 
 #get unique sampleID
 cut -f2 $IMAGEPATH"/sampleIDs.txt" | sort | uniq >> $IMAGEPATH"/sampleIDs_uniqe.txt"
 
 #compile list for each sampleID
+
 while read j; do grep "\<$j\>" $IMAGEPATH"/sampleIDs.txt" > $IMAGEPATH"/sample_$j.txt"; done < $IMAGEPATH"/sampleIDs_uniqe.txt"
 
 #create a mosaic
+
 
