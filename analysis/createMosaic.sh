6b63e322f669de432d577c376d06c840ea4441f8
gabe0815 <gabriel.schweighauser@unibas.ch>
2015-12-11 19:26:07 +0100

    finished sorting

--- analysis/createMosaic.sh
+++ analysis/createMosaic.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#function assembleMosaic {
+	
+#}
+
 #IMAGEPATH is stored in config.sh to make it accessible to all scripts
-. config.sh
+. ~/applications/RAPID/analysis/config.sh
 
 #get sample list 
 if [ -f $IMAGEPATH/sampleIDs.txt ]; then
@@ -10,14 +14,24 @@ fi
 
 
 #exclude all sets that have been recorded before 2015-12-06 17:30:00 as until then, the assay was not working properly.
+#include only sets that have been analysed
+#to calculate minutes, use $(echo 'scale=$TIMEDIFF/3600' | bc -l)
+
 #hence $BADTIME is the lower limit
 BADTIME=$(date --date="2015-12-06 17:30:00" +%s)
 
 for i in $(find $IMAGEPATH -name "sampleID.txt"); 
 do 
-	if [ $(tail -n1 $i) -ge $BADTIME ] then
-		echo -en $i'\t'; head -n1 $i; 
+	DIRNAME=$(dirname $i)
+	TIMESTAMP=$(cat $DIRNAME"/timestamp.txt")
+	TIMEOFBIRTH=$(tail -n1 $i)
+	SAMPLEID=$(head -n1 $i)
+
+	if [ $TIMESTAMP -ge $BADTIME ] && [ -f $DIRNAME"/imgseries_h264.AVI_parameters.txt" ]; then
+#		echo -en $i'\t'; head -n1 $i; echo -en "\t $((TIMESTAMP-TIMEOFBIRTH)) 
+		printf "$i\t$SAMPLEID\t$((TIMESTAMP-TIMEOFBIRTH))\n"	
 	fi
+
 done >> $IMAGEPATH"/sampleIDs.txt"
 
 
@@ -26,6 +40,7 @@ cut -f2 $IMAGEPATH"/sampleIDs.txt" | sort | uniq >> $IMAGEPATH"/sampleIDs_uniqe.
 
 #compile list for each sampleID
 
+#while read j; do grep "\<$j\>" $IMAGEPATH"/sampleIDs.txt" > $IMAGEPATH"/sample_$j.txt"; assembleMosaic $IMAGEPATH"/sample_$j.txt"; done < $IMAGEPATH"/sampleIDs_uniqe.txt"
 while read j; do grep "\<$j\>" $IMAGEPATH"/sampleIDs.txt" > $IMAGEPATH"/sample_$j.txt"; done < $IMAGEPATH"/sampleIDs_uniqe.txt"
 
 #create a mosaic
