be7531d0aaa98d3b3082b8544fc0bcaf0e441914
gabe0815 <g.schweighauser@gmail.com>
2016-04-05 22:07:11 +0200

    fixed countour picking up boarders of image and add overlay to color image

--- analysis/trackLength.py
+++ analysis/trackLength.py
@@ -14,14 +14,11 @@ def threshold(parentDir, trackFile, description):
     #print thisImage
     img = cv2.imread(thisImage,0)
     img = cv2.medianBlur(img,15)    
-    height, width = img.shape[:2]
-
 
     #thresholding
-    th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,15,2) 
-    contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    area = (height*width) - cv2.countNonZero(th)    
-    trackFile.write('\n'+description+'\t'+str(0)+'\t'+str(area))
+    th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,15,2) 
+    contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
+    trackFile.write('\n'+description+'\t'+str(0)+'\t'+str(cv2.countNonZero(th)))
     
 
     if description == "after":
@@ -30,16 +27,15 @@ def threshold(parentDir, trackFile, description):
                 print f
                 thisImage = parentDir + f
                 #overlay countour and area
-                img = cv2.imread(thisImage,0)            
-                overlay = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-                cv2.drawContours(overlay, contours, -1, (0,0,255), 1)
-                cv2.putText(overlay, str(area), (100,2200), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 10)  
-                cv2.imwrite(thisImage+"_tracklength.jpg", overlay)
+                img = cv2.imread(thisImage)            
+                cv2.drawContours(img, contours, -1, (0,0,255), 1)
+                cv2.putText(img, str(cv2.countNonZero(th)), (100,2200), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 10)  
+                cv2.imwrite(thisImage+"_tracklength.jpg", img)
    
 
             
 #src = sys.argv[1]
-src = '/media/imagesets04/20160321_FIJI_analysis_testing/dl1455724911_2_1_1/'
+src = '/home/gabe/OldAlbert/media/4TBexternal/sync/imagesets/dl1455724911_2_1_1/'
 
 try:
     os.remove(src + "trackLength.tsv")
