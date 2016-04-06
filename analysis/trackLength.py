c510fdd64833661e2b3e2fea52acd3fbd7fa5667
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-04-06 18:16:35 +0200

    do not threshold if there seems to be no track

--- analysis/trackLength.py
+++ analysis/trackLength.py
@@ -15,13 +15,18 @@ def threshold(parentDir, trackFile, description):
             thisImage = parentDir + f
    
     img = cv2.imread(thisImage,0)
-    img = cv2.medianBlur(img,15)    
-
-    #thresholding
-    th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,15,2) 
-    contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
-    trackFile.write('\n'+description+'\t'+str(0)+'\t'+str(cv2.countNonZero(th)))
+    img = cv2.medianBlur(img,15)
+    #get minimum
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(img)
+    if minVal > 200:
+        trackFile.write('\n'+description+'\t'+str(0)+'\t'+str(0))
     
+    else:
+        #thresholding
+        th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,15,2) 
+        contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
+        trackFile.write('\n'+description+'\t'+str(0)+'\t'+str(cv2.countNonZero(th)))
+        
 
     if description == "after":
         for f in os.listdir(parentDir):
@@ -29,12 +34,17 @@ def threshold(parentDir, trackFile, description):
                 thisImage = parentDir + f
                 #overlay countour and area
                 img = cv2.imread(thisImage)            
-                cv2.drawContours(img, contours, -1, (0,0,255), 1)
-                cv2.putText(img, str(cv2.countNonZero(th)), (100,2200), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 10)  
-                cv2.imwrite(thisImage+"_tracklength.jpg", img)
+
+                if minVal > 200:
+                    cv2.putText(img, str(0), (100,2200), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 10)  
+                else:
+                    cv2.drawContours(img, contours, -1, (0,0,255), 1)
+                    cv2.putText(img, str(cv2.countNonZero(th)), (100,2200), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 10)                      
+
+        cv2.imwrite(thisImage+"_tracklength.jpg", img)
    
 src = sys.argv[1]
-#src = "/media/imagesets04/20160311_vibassay_set5/dl1457762574_3_4_0/"
+#src = "/media/imagesets04/20160311_vibassay_set5/dl1457709627_6_1_2/"
 try:
     os.remove(src + "trackLength.tsv")
 except OSError:
