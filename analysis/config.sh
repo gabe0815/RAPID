5a06d20cb7926f1b9866de2c6ddd8d0c4bc82362
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-05-04 17:53:53 +0200

    changed path

--- analysis/config.sh
+++ analysis/config.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#IMAGEPATH=/mnt/4TBraid02/20151203_vibassay_set2
-#IMAGEPATH=/mnt/4TBraid02/20160122_vibassay_set3
-IMAGEPATH=/mnt/4TBraid02/20160217_vibassay_set4
+IMAGEPATH=/mnt/4TBraid02/20160504_vibassay_set7
