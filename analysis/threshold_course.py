24fac32fe3af2e76a6b0e58381d05e92d7382794
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-07-07 18:16:50 +0200

    added options

--- analysis/threshold_course.py
+++ analysis/threshold_course.py
@@ -4,28 +4,19 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-origImg = cv2.imread('/home/user/thresholding/imgseries_h264.AVI_2fps.AVI_27_55_after.jpg',0)
+origImg = cv2.imread('/home/jhench/mac/Documents/sync/lab_journal/2016/data201607/trackthresholding/noise.jpg',0)
 
-img = cv2.medianBlur(origImg,15)
-th1 = cv2.medianBlur(origImg,27)
-th2 = cv2.medianBlur(origImg,31)
-th3 = cv2.medianBlur(origImg,35)
-
-
-img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,15,2)
-th1 = cv2.adaptiveThreshold(th1,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,15,2)
-th2 = cv2.adaptiveThreshold(th2,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,15,2)
-th3 = cv2.adaptiveThreshold(th3,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,15,2)
-
-titles = ['median blur 15', 'median blur 27', 'median blur 31', 'median blur 35']
-images = [img, th1, th2, th3]
-
-for i in xrange(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+kernel = np.ones((5,5),np.uint8)
+titles = ['median blur 15', 'median blur 21', 'median blur 27', 'median blur 31']
+sizes = [15, 21, 27, 31]
+for i in range(4):
+    img = cv2.medianBlur(origImg,sizes[i])
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,15,2)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations = 2)
+    plt.subplot(2,2,i+1),plt.imshow(img,'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
-plt.show()
+plt.savefig("/mnt/1TBraid01/homefolders/gschweighauser/median_blur_opening_2_noise.png", bbox_inches='tight', format='png', dpi=300)
+#plt.show()
+
+
