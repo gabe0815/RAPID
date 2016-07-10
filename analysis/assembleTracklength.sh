d668f0f848e33a31fc0050ba290526670ea257ea
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-07-10 17:29:18 +0200

    fixed filepath

--- analysis/assembleTracklength.sh
+++ analysis/assembleTracklength.sh
@@ -14,15 +14,16 @@ SCALE=0.25
 #functions
 function assembleMosaic {
     imglist=""
+    filepath="${1%.*}"
   	filename=$(basename "$1")
 	sampleID="${filename%.*}" 
     while read i;
 		do
             img=$( echo "$i" | cut -f1); 
-            imglist=$imglist $img
+            imglist=$(echo $imglist $img)
     
     done < $1
-    montage $imglist -tile "$COLUMNS"x -geometry $(echo "$WIDTH"*"$SCALE"/1 | bc)x$(echo "$HEIGTH"*"$SCALE"/1 | bc) -title $sampleID $IMAGEPATH"_montage_tracklength.jpg"
+    montage $imglist -tile "$COLUMNS"x -geometry $(echo "$WIDTH"*"$SCALE"/1 | bc)x$(echo "$HEIGTH"*"$SCALE"/1 | bc) -title $sampleID $filepath'_montage_tracklength.jpg'
 
     
 }
