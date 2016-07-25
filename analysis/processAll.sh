dc82b9081054fe0e7d70343c50d80479c935afa9
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-07-25 17:53:38 +0200

    progress

--- analysis/processAll.sh
+++ analysis/processAll.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 while read i; 
 do  
+    echo "processing $i";
     ls -d "$i"/*/ |  parallel --eta -j16 "/mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/trackLength.py {}"; 
+    echo "assembly of $i";
     /mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/assembleTracklength.sh $i;
 
 done < $1
