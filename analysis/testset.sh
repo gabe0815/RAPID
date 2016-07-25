1a472733307d975f123500c11d9d68bed96fa959
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-07-25 16:15:50 +0200

    added mosaic censoring table

--- analysis/testset.sh
+++ analysis/testset.sh
@@ -5,7 +5,7 @@
 TESTSET=/media/imagesets04/20160311_vibassay_set5/IFP199_12_folders.txt
 TESTSETLIST=/media/imagesets04/20160311_vibassay_set5/IFP199_12_sorted.txt
 #run tracklength.py
-cat $TESTSET | parallel --eta -j16 python /mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/trackLength.py {}
+#cat $TESTSET | parallel --eta -j16 python /mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/trackLength.py {}
 
 #assemble mosaic:
 WIDTH=3072
@@ -18,11 +18,19 @@ function assembleMosaic {
     imglist=""
     filepath="${1%.*}"
   	filename=$(basename "$1")
-	sampleID="${filename%.*}" 
+	sampleID="${filename%.*}"
+    > $filepath'_mosaic_coordinates.txt'
+    count=0 
     while read i;
 		do
             img=$( echo "$i" | cut -f1); 
             imglist=$(echo $imglist $img)
+            xcoord=$(echo $count%$COLUMNS | bc)
+            ycoord=$(echo $count/$COLUMNS | bc)
+            echo $img','$xcoord','$ycoord 
+            echo $img','$xcoord','$ycoord >> $filepath'_mosaic_coordinates.txt'
+            count=$(echo "$count +1" | bc)
+
     
     done < $1
     montage $imglist -tile "$COLUMNS"x -geometry $(echo "$WIDTH"*"$SCALE"/1 | bc)x$(echo "$HEIGTH"*"$SCALE"/1 | bc) -title $sampleID $filepath'_montage_testset.jpg'
