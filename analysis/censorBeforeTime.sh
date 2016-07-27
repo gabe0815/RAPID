fb07e848587e59747bca833c70663e2afb32d78c
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-07-27 14:17:31 +0200

    completed functionality

--- analysis/censorBeforeTime.sh
+++ analysis/censorBeforeTime.sh
@@ -2,7 +2,20 @@
 
 BADTIME=$(date --date="2015-12-06 17:30:00" +%s)
 
-for i in $(find $1 -name "timestamp.txt"); 
-do 
-    echo $i
-done
+censor_bad_time(){
+    time=$(head -n1 $1)
+    if [ $time -lt $BADTIME ];
+    then
+        filepath="${1%.*}"
+        echo "censored" > $filepath"/censored.txt"
+        echo "true"
+    else 
+        echo "false"
+    fi
+}
+export -f censor_bad_time
+export BADTIME
+
+#find $1 -name "timestamp.txt" | parallel "if [ $(head -n1 {}) < $BADTIME ]; then echo true; fi" 
+find $1 -name "timestamp.txt" | parallel censor_bad_time {}
+
