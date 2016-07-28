06c251700b6f2c1c885ae5a819c4a9f0269d3b54
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-07-28 10:05:19 +0200

    fixed

--- analysis/createOverlay.sh
+++ analysis/createOverlay.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 combine(){
-    if [ ! -f "*overlay.jpg" ]
+    path=$(dirname $1)    
+    
+    if [ ! -f $path"/imgseries_h264.AVI_2fps.AVI_overlay.jpg" ]
     then 
-        path=$(dirname $1)
         photo=$(find $path -name "*_[0-9][0-9].jpg")
-        ./mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/combineTrackAndPhoto.py $1 $photo    
+        echo "processing: $path"
+        /mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/combineTrackAndPhoto.py $1 $photo    
         #echo $1 $photo
         #echo "********************"
+    else
+        echo "skipping ..."    
     fi
 }
 
