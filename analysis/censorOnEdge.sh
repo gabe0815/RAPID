deb8c236032b82db36a3982dc9ee0f84d9a37eb8
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-08-01 14:14:06 +0200

    censor on edge

---
+++ analysis/censorOnEdge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+censor_onEdge(){
+    
+    if [ $(grep "after" $1 | cut -f4) -eq 1 ];
+    then 
+        filepath=$(echo $1 | cut -d "/" -f1-5)
+        echo "true"        
+        #echo $filepath
+        #echo "censored" > $filepath"/censored.txt"
+    else
+        echo "false"
+    fi
+}
+
+export -f censor_onEdge()
+
+find $1 -name "trackLength.tsv" | parallel censor_bad_time {}
+
