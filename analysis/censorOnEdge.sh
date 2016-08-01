e099dbde292943839914baa9312639fc8fba1859
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-08-01 14:20:11 +0200

    verbose

--- analysis/censorOnEdge.sh
+++ analysis/censorOnEdge.sh
@@ -5,7 +5,8 @@ censor_onEdge(){
     if [ $(grep "after" $1 | cut -f4) -eq 1 ];
     then 
         filepath=$(echo $1 | cut -d "/" -f1-5)
-        echo "censored" > $filepath"/censored.txt"
+        echo "$filepath is on edge..."
+	echo "censored" > $filepath"/censored.txt"
     fi
 }
 
