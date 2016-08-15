5b8df3b6adada8f6a2af947e399e6f0189f7e9c4
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-08-15 11:25:49 +0200

    path for rsync is now determined by the script

--- analysis/backup_results.sh
+++ analysis/backup_results.sh
@@ -2,6 +2,7 @@
 
 . ~/applications/RAPID/analysis/config.sh
 
+DIR=$(echo $IMAGEPATH | cut -d "/" -f4)
 cd $IMAGEPATH
 #rsync -aP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/20160311_vibassay_set5/
-rsync -aP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/20160720_vibassay_set9
+rsync -aP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
