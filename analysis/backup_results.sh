d02df315eebb041791049d72d6f427b77597f18d
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-08-18 13:50:17 +0200

    bwlimit disabled

--- analysis/backup_results.sh
+++ analysis/backup_results.sh
@@ -4,5 +4,5 @@
 
 DIR=$(echo $IMAGEPATH | cut -d "/" -f4)
 cd $IMAGEPATH
-#rsync -aP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/20160311_vibassay_set5/
-rsync -aP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
+#rsync -aP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
+rsync -aP --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
