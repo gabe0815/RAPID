670041a4254925f4410faf8cb64fdc2e9231694f
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-08-18 13:58:07 +0200

    adapt path to host

--- analysis/config.sh
+++ analysis/config.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-IMAGEPATH=/media/imagesets04/20160217_vibassay_set4
+if [ $(hostname) == "ws522988" ];
+then 
+	IMAGEPATH=/mnt/4TBraid04/imagesets04/20160810_vibassay_set10
+else
+	IMAGEPATH=/mnt/4TBraid02/20160810_vibassay_set10
+fi
