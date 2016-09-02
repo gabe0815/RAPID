c1c56ef416a54cc495a505afea824fcd6ef77017
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-09-02 15:38:01 +0200

    added sticky flag

--- analysis/backup_results.sh
+++ analysis/backup_results.sh
@@ -5,4 +5,4 @@
 DIR=$(echo $IMAGEPATH | cut -d "/" -f4)
 cd $IMAGEPATH
 #rsync -rtlP --bwlimit=1000 --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
-rsync -rtlP --chmod=g+rw -p --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
+rsync -rtlP --chmod=g+rws -p --exclude='*.AVI' --exclude="*.JPG" --include='*' ./ jhench@ws522988.dyn.uhbs.ch:/mnt/4TBraid04/imagesets04/"$DIR"
