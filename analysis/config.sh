0211007944da9be5906a3f55c50cac52ccefe83b
gabe0815 <gabriel.schweighauser@unibas.ch>
2016-09-08 09:49:34 +0200

    changed path

--- analysis/config.sh
+++ analysis/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $(hostname) == "ws522988" ];
 then 
-	IMAGEPATH=/mnt/4TBraid04/imagesets04/20160810_vibassay_set10
+	IMAGEPATH=/mnt/4TBraid04/imagesets04/20160902_vibassay_set11
 else
-	IMAGEPATH=/mnt/4TBraid02/20160810_vibassay_set10
+	IMAGEPATH=/mnt/4TBraid02/20160902_vibassay_set11
 fi
