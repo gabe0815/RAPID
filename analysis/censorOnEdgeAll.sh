72e2bcc652bb38d16e95ecc8b8b8bf7463b8d149
gabe0815 <g.schweighauser@gmail.com>
2017-04-13 13:52:14 +0200

    merged experimental with master and cleaned up the repo. Removed all unnecessary files

--- analysis/censorOnEdgeAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read i; 
-do  
-     echo "censor on edge in $i";
-     /mnt/1TBraid01/homefolders/gschweighauser/RAPID/analysis/censorOnEdge.sh $i;
-
-done < $1
-
--- analysis/renameSamples.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-parallel -a /mnt/4TBraid04/imagesets04/20160810_vibassay_set10/sample_N2.FUdR."$1"_sorted_montage_tracklength.jpg_wrongID.txt  "sed -i s/N2\.FUdR\.$1/N2\.FUdR\.$2/g {}/sampleID.txt"
-
-parallel -a /mnt/4TBraid04/imagesets04/20160810_vibassay_set10/sample_N2.FUdR."$2"_sorted_montage_tracklength.jpg_wrongID.txt  "sed -i s/N2\.FUdR\.$2/N2\.FUdR\.$1/g {}/sampleID.txt"
